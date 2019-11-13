07781749432af7e925c4cc0b9ccc6d5de825a5db
iampreethamveigas <veigasbros@gmail.com>
2019-11-13 10:45:19 +0530

    ignore file

--- app/register/register.routing.module.ts
+++ app/register/register.routing.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
-import {RegisterComponent} from './register.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register.component';
 
 
 
-const registerRoutes : Routes=[
-    {path:'Register',component:RegisterComponent}
+const registerRoutes: Routes = [
+    { path: 'Register', component: RegisterComponent }
 ];
 
 @NgModule({
-    imports:[RouterModule.forChild(registerRoutes)],
-    exports:[RouterModule]
+    imports: [RouterModule.forChild(registerRoutes)],
+    exports: [RouterModule]
 })
 
 export class RegisterRoutingModule { }
