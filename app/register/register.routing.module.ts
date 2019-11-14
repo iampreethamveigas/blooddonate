5db8b5252b1c13a79efc216cbef370888de96c0d
iampreethamveigas <veigasbros@gmail.com>
2019-11-14 10:49:21 +0530

    remove space

--- app/register/register.routing.module.ts
+++ app/register/register.routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register.component';
 
-
-
 const registerRoutes: Routes = [
     { path: 'Register', component: RegisterComponent }
 ];
